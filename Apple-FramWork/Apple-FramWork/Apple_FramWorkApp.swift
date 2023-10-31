b7708196b39128261848d2b4892ad371aa70b9dc
lov <ratedl51@gmail.com>
2023-10-31 22:51:34 +0530

    Refactor location

---
+++ Apple-FramWork/Apple-FramWork/Apple_FramWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FramWorkApp.swift
+//  Apple-FramWork
+//
+//  Created by LOVE  on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FramWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
